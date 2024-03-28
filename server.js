a80a4ed50682d2bcb918ee6c9ec57ae0065c1def
iuh20039561 <153615560+iuh20039561@users.noreply.github.com>
2024-03-28 15:28:32 +0700

    server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const HOST = '0.0.0.0';
 
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello MR. HUYNH NAM');
+  res.send('hello! Student code is X');
 });
 
 app.listen(PORT, HOST);
